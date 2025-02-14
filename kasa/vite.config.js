9b572f465b8c901b3fddae921ccf26d1e25469d6
Oumaima <oumaimaelyb@gmail.com>
2025-02-14 17:30:51 +0100

    :update et redeploiment

--- kasa/vite.config.js
+++ kasa/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kasa/",
+  base: "/home/",
   server: {
     open: true, // Ouvre automatiquement la page dans le navigateur
   },
