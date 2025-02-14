4842cda91e93f391d07127867ce383c169695615
Oumaima <oumaimaelyb@gmail.com>
2025-02-14 18:48:56 +0100

    Build pour déploiement sur GitHub Pages

--- kasa/vite.config.js
+++ kasa/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/home/",
+  base: "/kasa/",
   server: {
     open: true, // Ouvre automatiquement la page dans le navigateur
   },
